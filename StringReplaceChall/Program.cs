32b182ac525efc84f413c770d59b81a063bbf1da
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-06 19:32:01 +0200

    UI example code
    created example code to show replace text method work.

--- StringReplaceChall/Program.cs
+++ StringReplaceChall/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using StringReplaceChall;
+
+StringReplaceProcessor srp = new();
+
+Console.Write("Write text to search for: ");
+string oldText = Console.ReadLine();
+
+Console.Write("Write text to replace old text: ");
+string newText = Console.ReadLine();
+
+srp.ReplaceText(oldText, newText, "primary.txt");
+
