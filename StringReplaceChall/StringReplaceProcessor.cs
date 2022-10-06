2c2258d3213a4ae1c73dee6a1ce249641ccbe310
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-06 19:31:26 +0200

    Replace Text Method
    Created  method to replace old text from text file with new one, that are given via parameters. Also created 2 methods, for reading text from file, and for saving text to file.

--- StringReplaceChall/StringReplaceProcessor.cs
+++ StringReplaceChall/StringReplaceProcessor.cs
@@ -8,6 +8,55 @@ namespace StringReplaceChall
 {
     internal class StringReplaceProcessor
     {
+        private string fullText;
+
+        public void ReplaceText(string oldText, string newText, string textPath)
+        {
+            List<List<string>> fullText = ReadTextFromFile(textPath);
+
+            foreach (var line in fullText)
+            {
+
+                for (int i = 0; i < line.Count; i++)
+                {
+                    if (line[i].Contains(oldText))
+                    {
+                        int index = line.FindIndex(x => x == line[i]);
+                        if (index != -1)
+                        {
+                            line[index] = line[index].Replace(oldText, newText);
+                        }
+                    }
+                }
+
+            }
+            SaveTextToFile(fullText, textPath);
+        }
         
+        private void SaveTextToFile(List<List<string>> textToSave, string textPath)
+        {
+            List<string> lines = new();
+
+            foreach (var line in textToSave)
+            {
+                lines.Add(String.Join(" ", line));
+            }
+
+            File.WriteAllLines(textPath, lines);
+        }
+
+        private List<List<string>> ReadTextFromFile(string textPath)
+        {
+            List<List<string>> output = new();
+
+            List<string> text = File.ReadAllLines(textPath).ToList();
+
+            text.ForEach(line =>
+            {
+                output.Add(line.Split(' ').ToList());
+            });
+
+            return output;
+        }
     }
 }
