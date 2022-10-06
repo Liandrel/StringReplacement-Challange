041e89b5005079794ff4c1054cbef6212091e813
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-06 20:01:28 +0200

    UI example code
    added example code to console UI to show ReplaceParameterwithValue method work, added method that is used as delegate

--- StringReplaceChall/Program.cs
+++ StringReplaceChall/Program.cs
@@ -3,11 +3,22 @@ using StringReplaceChall;
 
 StringReplaceProcessor srp = new();
 
-Console.Write("Write text to search for: ");
-string oldText = Console.ReadLine();
+//Console.Write("Write text to search for: ");
+//string oldText = Console.ReadLine();
 
-Console.Write("Write text to replace old text: ");
-string newText = Console.ReadLine();
+//Console.Write("Write text to replace old text: ");
+//string newText = Console.ReadLine();
 
-srp.ReplaceText(oldText, newText, "primary.txt");
+//srp.ReplaceText(oldText, newText, "primary.txt");
 
+srp.ReplaceParametersWithText("bonus.txt", AskForParameterValue);
+
+string AskForParameterValue(string parameter)
+{
+    string output = "";
+
+    Console.Write($"what is the value of {parameter}: ");
+    output = Console.ReadLine();
+
+    return output;
+}
