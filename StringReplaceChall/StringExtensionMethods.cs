2d21b3888a9f686faf77440d8bc7be828b23eb0d
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-06 19:58:28 +0200

    String extension methods
    Added class with string extension method for finding string between two specified characters

---
+++ StringReplaceChall/StringExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReplaceChall
+{
+    internal static class StringExtensionMethods
+    {
+        public static string Between(this string src, string findfrom, string findto)
+        {
+            int start = src.IndexOf(findfrom);
+            int to = src.IndexOf(findto, start + findfrom.Length);
+            if (start < 0 || to < 0) return "";
+            string s = src.Substring(
+                           start + findfrom.Length,
+                           to - start - findfrom.Length);
+            return s;
+        }
+    }
+}
